e43f23ac4c759626ee029f307b6b7e08aa78f8fb
Bill Cavanagh <billcavanagh38@gmail.com>
2024-05-19 17:38:03 -0400

    created basic algorithm for determining board value

--- src/main/java/chess/Bot/BoardValue.java
+++ src/main/java/chess/Bot/BoardValue.java
@@ -1,7 +1,40 @@
 package chess.Bot;
+
+import chess.Board;
+import chess.Color;
+import chess.Pieces.*;
+
 /**
  * Class that contains varying algorithms for determining the "value" of a board state for each player
  */
 public class BoardValue {
-    
+    public Position getPosition(Board board, BoardValueType valueType){
+        Position position = new Position(board, valueType);
+        int[] temp = new int[2];
+        switch(valueType){
+            case PIECES_VALUE:
+                position.setAdvantage(piecesValue(board));
+            default:
+                position.setAdvantage(0);
+        }
+        return position;
+    }
+    /**
+     * Determines the value for a position based on raw value of pieces
+     * <p> iterates through all pieces 
+     */
+    public double piecesValue(Board board){
+        double value = 0;
+        for(DefaultPiece piece : board.whitePieces){
+            if (!(piece instanceof King)){
+                value += piece.getValue();
+            }
+        }
+        for(DefaultPiece piece : board.blackPieces){
+            if (!(piece instanceof King)){
+                value -= piece.getValue();
+            }
+        }
+        return value;
+    }
 }
--- src/main/java/chess/Bot/Position.java
+++ src/main/java/chess/Bot/Position.java
@@ -4,17 +4,35 @@ import chess.Board;
 import chess.Color;
 
 /**
- * Class that contains information about a position with corresponding methods
+ * Class that contains information about a position
  */
-public class Position {s
+public class Position {
+    /**
+     * The associated board state
+     */
     public Board board;
-    public Color advantage;
-    public double value;
+    /**
+     * The value of the position, negative indicates black advantage, positive indicates white advantage
+     */
+    public double advantage;
+    /**
+     * The algorithm that was used to determine the value of the position
+     */
     public BoardValueType valueType;
-    public static BoardValueType valueTypes[] = BoardValueType.values();
     public Position(Board board, BoardValueType valueType){
         this.board = board;
         this.valueType = valueType;
+    }
+    public Position(Board board, BoardValueType valueType, double advantage){
+        this.board = board;
+        this.valueType = valueType;
+        this.advantage = advantage;
+    }
+    public void setAdvantage(double advantage){
+        this.advantage = advantage;
+    }   
+    public double getAdvantage(){
+        return advantage;
     }
 
 }
